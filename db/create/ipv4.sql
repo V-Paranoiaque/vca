406bbb4ec34871ffacbc8deecb24d58134cc4e40
Mighty <mightykiller2b@gmail.com>
2014-12-12 20:07:21 +0100

    By default ip are not attributed

--- db/create/ipv4.sql
+++ db/create/ipv4.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `ipv4` (
   `ip` varchar(15) NOT NULL,
-  `ip_owner` int(10) unsigned NOT NULL
+  `ip_owner` int(10) unsigned NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `ipv4`
