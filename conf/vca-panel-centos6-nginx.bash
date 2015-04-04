8d3800302d513086c19e177076e132e41b8f5b72
Virgil <mightykiller2b@gmail.com>
2015-04-04 16:59:43 +0200

    Use repo to install epel repo

--- conf/vca-panel-centos6-nginx.bash
+++ conf/vca-panel-centos6-nginx.bash
@@ -4,11 +4,7 @@ if [ ! -d "/usr/share/vca/www" ];then
 	exit 0
 fi
 
-yum install wget -y
-
-wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-rpm -ivh epel-release-6-8.noarch.rpm
-rm -f epel-release-6-8.noarch.rpm
+yum install epel-release -y
 
 yum install nginx php-fpm mysql-server php-mysql php-mcrypt gettext openssl -y
 
