a0cbfb45ec914deb16d78935992537d95d84a7fb
Mighty <mightykiller2b@gmail.com>
2015-04-16 21:16:12 +0200

    Use a file for all include

--- www/templates/basic/form_backupscheduledelete.php
+++ www/templates/basic/form_backupscheduledelete.php
@@ -1,16 +1,6 @@
 <?php
 
-include('../../config.php');
-include('../../functions.php');
-include('../../libs/Paquet.class.php');
-
-include('../../libs/Db.class.php');
-include('../../libs/Socket.class.php');
-include('../../libs/Vps.class.php');
-include('../../libs/Server.class.php');
-include('../../libs/Guest.class.php');
-include('../../libs/User.class.php');
-include('../../libs/Admin.class.php');
+include('header.php');
 
 if(!empty($_GET['saveId']) && !empty($_GET['vps'])) {
 	$paquet = new Paquet();
---
+++ www/templates/basic/header.php
@@ -0,0 +1,15 @@
+<?php 
+
+include('../../config.php');
+include('../../functions.php');
+include('../../libs/Paquet.class.php');
+
+include('../../libs/Db.class.php');
+include('../../libs/Socket.class.php');
+include('../../libs/Vps.class.php');
+include('../../libs/Server.class.php');
+include('../../libs/Guest.class.php');
+include('../../libs/User.class.php');
+include('../../libs/Admin.class.php');
+
+?>
